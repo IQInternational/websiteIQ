f214f4791c14bb88327df64b7a24ad6f7ddc6bf0
IQInternational <limreaksa41@gmail.com>
2026-07-01 20:18:09 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // Replace 'YOUR-REPO-NAME' with your actual repository name on GitHub
-  base: 'websiteIQ', 
+  // Change it to this:
+  base: '/websiteIQ/', 
 })
