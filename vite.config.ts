b6a84c6243db682e4a2f52f962df39775c045a09
IQInternational <limreaksa41@gmail.com>
2026-07-01 21:43:23 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // Bolt projects usually need this for GitHub Pages
-  base: './', 
+  // This tells Vite to look for assets relative to the repository folder
+  base: '/IQTeacherDay/',
 })
