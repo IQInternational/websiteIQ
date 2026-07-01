fdb13e3c355b062fbf1c413f1a6840df9210d4fc
IQInternational <limreaksa41@gmail.com>
2026-07-01 20:16:55 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-});
+  // Replace 'YOUR-REPO-NAME' with your actual repository name on GitHub
+  base: 'websiteIQ', 
+})
